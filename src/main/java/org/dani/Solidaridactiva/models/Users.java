35189e5ca6c8d7b7f9ee1fc551d39f5d460e8b64
dani martin <danimartinjuarez@gmail.com>
2023-03-07 18:03:39 +0100

    fix errors

--- src/main/java/org/dani/Solidaridactiva/models/Users.java
+++ src/main/java/org/dani/Solidaridactiva/models/Users.java
@@ -16,6 +16,13 @@ public class Users {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    private String whatLike;
+    private String whatOffer;
+    private String location;
+    private String activityType;
+    private String contact;
+    private String photo;
+    private String title;
 
 
 }
